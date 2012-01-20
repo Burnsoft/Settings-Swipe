82ea9250eb5b1a8f5b8aace24a9e5c1192b7f1a6
Nik Burns <nik@burnsoft.co.uk>
2012-01-20 22:54:43 +0000

    Initial Commit

---
+++ Settings Swipe/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Settings Swipe
+//
+//  Created by Nicholas Burns on 20/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Settings Swipe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Settings Swipe
+//
+//  Created by Nicholas Burns on 20/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
